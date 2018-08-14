23c4190b5a091f4df6529e8598fe47069aff1e75
Miguel Ferrer <miguef7@gmail.com>
2018-08-13 21:10:46 -0500

    refactor test to made them more roboust

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -9,26 +9,31 @@ RSpec.describe Property, type: :model do
   it "is invalid without a property_type" do
     property = FactoryBot.build(:property, property_type: nil)
     expect(property).to_not be_valid
+    expect(property.errors.messages).to have_key(:property_type)
   end
 
   it "is invalid without a name" do
     property = FactoryBot.build(:property, name: nil)
     expect(property).to_not be_valid
+        expect(property.errors.messages).to have_key(:name)
   end
 
   it "is invalid without a building_coeficient" do
     property = FactoryBot.build(:property, building_coeficient: nil)
     expect(property).to_not be_valid
+    expect(property.errors.messages).to have_key(:building_coeficient)
   end
 
   it "is invalid with a building_coeficient greater_than 1" do
     property = FactoryBot.build(:property, building_coeficient: 1.1)
     expect(property).to_not be_valid
+    expect(property.errors.messages).to have_key(:building_coeficient)
   end
 
   it "is invalid with a building_coeficient equal to 0" do
     property = FactoryBot.build(:property, building_coeficient: 0)
     expect(property).to_not be_valid
+    expect(property.errors.messages).to have_key(:building_coeficient)
   end
 
   it "is valid with a building_coeficient between to 0 and 1" do
@@ -39,6 +44,7 @@ RSpec.describe Property, type: :model do
   it "is invalid without a building" do
     property = FactoryBot.build(:property, building: nil)
     expect(property).to_not be_valid
+        expect(property.errors.messages).to have_key(:building)
   end
 
   context "shares relationship" do
