7797c6c6666319ffd5390fde28004fee047df69f
Miguel Ferrer <miguef7@gmail.com>
2018-08-13 22:06:52 -0500

    add share model test and validations to pass them

--- spec/models/share_spec.rb
+++ spec/models/share_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Share, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with property, owner, ownerability_percentage and payment_percentage" do
+    share = FactoryBot.build(:share)
+    expect(share).to be_valid
+  end
+
+  it "is invalid without property" do
+    share = FactoryBot.build(:share, property: nil)
+    expect(share).to_not be_valid
+    expect(share.errors.messages).to have_key(:property)
+  end
+
+  it "is invalid without owner" do
+    share = FactoryBot.build(:share, owner: nil)
+    expect(share).to_not be_valid
+    expect(share.errors.messages).to have_key(:owner)
+  end
+
+  it "is invalid without ownerability_percentage" do
+    share = FactoryBot.build(:share, ownerability_percentage: nil)
+    expect(share).to_not be_valid
+    expect(share.errors.messages).to have_key(:ownerability_percentage)
+  end
+
+  it "is invalid with ownerability_percentage = 0" do
+    share = FactoryBot.build(:share, ownerability_percentage: 0)
+    expect(share).to_not be_valid
+    expect(share.errors.messages).to have_key(:ownerability_percentage)
+  end
+
+  it "is invalid with ownerability_percentage > 1" do
+    share = FactoryBot.build(:share, ownerability_percentage: 1.1)
+    expect(share).to_not be_valid
+    expect(share.errors.messages).to have_key(:ownerability_percentage)
+  end
+
+  it "is invalid without payment_percentage" do
+    share = FactoryBot.build(:share, payment_percentage: nil)
+    expect(share).to_not be_valid
+    expect(share.errors.messages).to have_key(:payment_percentage)
+  end
+
+  it "is valid with payment_percentage = 0" do
+    share = FactoryBot.build(:share, payment_percentage: 0)
+    expect(share).to be_valid
+  end
+
+  it "is invalid with payment_percentage > 1" do
+    share = FactoryBot.build(:share, payment_percentage: 1.1)
+    expect(share).to_not be_valid
+    expect(share.errors.messages).to have_key(:payment_percentage)
+  end
 end
