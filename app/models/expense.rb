f738498677f0c925aa77ce1c00cd7bef09a6b0d5
Miguel Ferrer <miguef7@gmail.com>
2018-08-13 20:48:04 -0500

    rename enums of type columns with new names

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,6 @@
 class Expense < ApplicationRecord
   # TODO: agregar rubros al expense en el enum de abajo, OJO AL CAMBIAR EL ORDEN
-  enum type: ["Aseo", "Seguridad"]
+  enum category: ["Aseo", "Seguridad"]
   belongs_to :budget
 
   # TODO: add validations with TDD
