fad6db4f6858978436bd0916f7ae778e46081e9b
Miguel Ferrer <miguef7@gmail.com>
2018-08-13 20:49:18 -0500

    add tests to property model

--- spec/factories/properties.rb
+++ spec/factories/properties.rb
@@ -1,12 +1,32 @@
 FactoryBot.define do
   factory :property do
-    type "Apartamento"
-    sequence(:name) { |n| "#{self.type} #{n}" }
+    property_type "Apartamento"
+    sequence(:name) { |n| "#{property_type} #{n}" }
     phone "2660480"
     sequence(:matricula_inmobiliaria) { |n| "50566#{n}" }
-    building_coeficient 0.5
+    building_coeficient 0.1
     association :building
     area 50
     address "Calle 11a 42-18"
+
+    trait :type_house do
+      type "Casa"
+    end
+
+    trait :type_parking_space do
+      type "Parqueadero"
+    end
+
+    trait :fifty_fifty do
+      after(:create) do |property|
+        create_list(
+          :share,
+          2,
+          property: property,
+          ownerability_percentage: 0.5,
+          payment_percentage: 0.5
+        )
+      end
+    end
   end
 end
--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -5,4 +5,53 @@ RSpec.describe Property, type: :model do
     property = FactoryBot.build(:property)
     expect(property).to be_valid
   end
+
+  it "is invalid without a property_type" do
+    property = FactoryBot.build(:property, property_type: nil)
+    expect(property).to_not be_valid
+  end
+
+  it "is invalid without a name" do
+    property = FactoryBot.build(:property, name: nil)
+    expect(property).to_not be_valid
+  end
+
+  it "is invalid without a building_coeficient" do
+    property = FactoryBot.build(:property, building_coeficient: nil)
+    expect(property).to_not be_valid
+  end
+
+  it "is invalid with a building_coeficient greater_than 1" do
+    property = FactoryBot.build(:property, building_coeficient: 1.1)
+    expect(property).to_not be_valid
+  end
+
+  it "is invalid with a building_coeficient equal to 0" do
+    property = FactoryBot.build(:property, building_coeficient: 0)
+    expect(property).to_not be_valid
+  end
+
+  it "is valid with a building_coeficient between to 0 and 1" do
+    property = FactoryBot.build(:property, building_coeficient: 0.8)
+    expect(property).to be_valid
+  end
+
+  it "is invalid without a building" do
+    property = FactoryBot.build(:property, building: nil)
+    expect(property).to_not be_valid
+  end
+
+  context "shares relationship" do
+    it "can have many shares" do
+      property = FactoryBot.create(:property, :fifty_fifty)
+      expect(property.shares.size).to eq 2
+    end
+  end
+
+  context "owners relationship" do
+    it "can have many users" do
+      property = FactoryBot.create(:property, :fifty_fifty)
+      expect(property.owners.size).to eq 2
+    end
+  end
 end
