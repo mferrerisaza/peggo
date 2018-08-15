816a3dd243c87426e350a26d180637d696cef0ce
Miguel Ferrer <miguef7@gmail.com>
2018-08-15 09:21:32 -0500

    add basic seeds and modify some some models with dependent: :destroy condition

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,7 +1,7 @@
 class Building < ApplicationRecord
   belongs_to :user
-  has_many :properties
-  has_many :budgets
+  has_many :properties, dependent: :destroy
+  has_many :budgets, dependent: :destroy
   validates :name, presence: true
   validates :name, uniqueness: { scope: :user }
 
