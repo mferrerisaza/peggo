25e149a02d90da2dd4f149f4dbf75abbdd172b4b
Daniel Lopez <dlopez.10g@gmail.com>
2018-08-16 13:18:14 -0500

    spec config to sign in users

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,6 +39,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  config.include Warden::Test::Helpers
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -59,4 +60,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.after :each do
+    Warden.test_reset!
+  end
 end
