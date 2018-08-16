1a48612cc6c00bcd1326f37062a032a1578443d4
Daniel Lopez <dlopez.10g@gmail.com>
2018-08-16 13:11:28 -0500

    new & create actions in owners

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,14 +1,29 @@
 class OwnersController < ApplicationController
+
   def new
     @owner = Owner.new
+    # authorize @owner
   end
 
   def create
+    @owner = Owner.new(owner_params)
+    if @owner.save
+      redirect_to owners_path
+    else
+      render :new
+    end
   end
 
   def index
+    @owners = Owner.all
   end
 
   def show
+  end
+
+  private
+
+  def owner_params
+    params.require(:owner).permit(:name, :card_number, :phone, :email)
   end
 end
