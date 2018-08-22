1985b3fcb9a9561b3d489d7f4e2b56d4fa319f25
Daniel Lopez <dlopez.10g@gmail.com>
2018-08-22 16:16:14 -0500

    spec helper file

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,11 +1,2 @@
 require "rails_helper"
 
-describe Owner do
-  describe "helper" do
-  let(:owner) { Owner.new(name: Daniel, card_number: 102010202, phone: 2111111, email: dlopezsd@mai.com)}
-    it "returns correct percentage of ownerability" do
-    let(:owner) { Owner.new(share: 1) }
-      expect(owner.convert_share_to_percentage.to_s).to eq("100%")
-    end
-  end
-end
