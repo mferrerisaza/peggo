a7a375d36a396ecda72174a99c851f4db95349e5
Daniel Lopez <dlopez.10g@gmail.com>
2018-08-22 16:30:43 -0500

    changes in crete owner spec

--- spec/features/owners_spec.rb
+++ spec/features/owners_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.feature "Owners", type: :feature do
   scenario  "allows a user to create an owner" do
     user = FactoryBot.create(:user)
+    building = FactoryBot.create(:building)
     login_as(user, scope: :user)
-    visit new_owner_path
+    visit new_building_owner_path(building)
     fill_in "Nombre del propietario", with: "Daniel José López"
     fill_in "Cédula del propietario", with: "1040182859"
     fill_in "Teléfono del propietario", with: "2660480"
