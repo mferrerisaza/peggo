ba5936d8329b5829ff0f36b2cfe0ab993ce6aadc
Daniel Lopez <dlopez.10g@gmail.com>
2018-08-23 11:43:09 -0500

    a property has many owners

--- app/models/building.rb
+++ app/models/building.rb
@@ -2,6 +2,7 @@ class Building < ApplicationRecord
   belongs_to :user
   has_many :properties, dependent: :destroy
   has_many :budgets, dependent: :destroy
+  has_many :owners, through: :properties
   validates :name, presence: true
   validates :name, uniqueness: { scope: :user }
 
