5264a7cba7ff3e7bb9f17bfadf298a5bd542df2d
Daniel Lopez <dlopez.10g@gmail.com>
2018-08-24 20:34:04 -0500

    erase hello world

--- app/javascript/components/sidebar.js
+++ app/javascript/components/sidebar.js
@@ -5,18 +5,17 @@ function openNav() {
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  console.log("hello world")
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const closeBtns = document.querySelectorAll(".closebtn")
   const hambMenu = document.querySelector(".navbar-wagon-dropicon")
 
-  // if(hambMenu && closeBtns){
-  if(closeBtns){
-    // hambMenu.addEventListener("click", (event) => {
-    //   openNav();
-    // })
+  if(hambMenu && closeBtns){
+  // if(closeBtns){
+    hambMenu.addEventListener("click", (event) => {
+      openNav();
+    })
     for (let i = 0; i < closeBtns.length; i++) {
       closeBtns[i].addEventListener("click", (event) => {
         closeNav();
