e968eb4a25905eb2077c15c0518959d0d415cc47
Miguel Ferrer <miguef7@gmail.com>
2018-08-27 15:45:14 -0500

    add shares for error rendering

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -3,7 +3,8 @@ module SidebarHelper
     allowed_controllers = {
       buildings: %w[show],
       properties: %w[index show new create edit update],
-      owners: %w[index show]
+      owners: %w[index show],
+      shares: %w[create]
     }
     allowed_controllers[controller_name.to_sym].include?(action_name)
   end
