d6830267583d288029349cf8069bc4ca5baf49f9
Daniel Lopez <dlopez.10g@gmail.com>
2018-08-27 19:37:48 -0500

    a building has many bills

--- app/models/building.rb
+++ app/models/building.rb
@@ -3,10 +3,15 @@ class Building < ApplicationRecord
   has_many :properties, dependent: :destroy
   has_many :budgets, dependent: :destroy
   has_many :owners, through: :properties
+  has_many :bills, through: :properties
   validates :name, presence: true
   validates :name, uniqueness: { scope: :user }
 
   def building_coeficients_sum
     properties.sum(:building_coeficient)
+  end
+
+  def building_properties
+    self.properties
   end
 end
