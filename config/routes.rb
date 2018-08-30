b36688bbef52b1086458ff46d4a3f680ad070a6f
Daniel Lopez <dlopez.10g@gmail.com>
2018-08-30 18:11:54 -0500

    no route for bill index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :buildings, only: [:index, :show, :new, :create] do
     resources :properties
     resources :owners, only: [:index, :show, :new, :create]
-    resources :bills, only: [:index,  :new, :create]
+    resources :bills, only: [:new, :create]
   end
   resources :shares, only: [:create, :update, :destroy]
 end
