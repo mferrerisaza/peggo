e437b2f329d41dc590d70029eca74afda373f55a
Daniel Lopez <dlopez.10g@gmail.com>
2018-09-03 08:42:03 -0500

    no index needed on bills

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,5 +1,5 @@
 class BillsController < ApplicationController
-  before_action :set_building, only: %i[index show new create]
+  before_action :set_building, only: %i[show new create]
 
   def index
   end
@@ -12,23 +12,26 @@ class BillsController < ApplicationController
   end
 
   def create
-    raise
-    @bill = Building.new(bill_params)
-    authorize @bill
-    if @bill.save
-      redirect_to root_path
-    else
-      render 'new'
+    @properties = @building.building_properties
+    params[:bill].each do |b|
+      if b[:enviar].present?
+        @bill = Bill.new(bill_params(b))
+        authorize @bill
+        @bill.save
+      end
     end
+    render :new
   end
 
   private
 
-  def bill_params
-    params.require(:bill).permit(:name, :user_id)
+  def bill_params(params)
+    params.permit(:status, :share_id)
   end
 
   def set_building
     @building = Building.find(params[:building_id])
   end
+
+
 end
