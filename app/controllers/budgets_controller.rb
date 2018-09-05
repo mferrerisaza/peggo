b9ed0cdd4d430ee91a94e42571e05cbd981a8abb
Miguel Ferrer <miguef7@gmail.com>
2018-09-05 11:39:38 -0500

    cleaning

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -2,7 +2,7 @@ class BudgetsController < ApplicationController
   before_action :set_building, only: :index
 
   def index
-    @bugets = policy_scope(Budget.where(building: @building))
     authorize @building, :building_of_current_user?
+    @bugets = policy_scope(Budget.where(building: @building))
   end
 end
