19d4db9567e3cafb9965d1b8afd40375f9c38cbd
Miguel Ferrer <miguef7@gmail.com>
2018-09-06 12:59:16 -0500

    policy for destroying a budget

--- app/policies/budget_policy.rb
+++ app/policies/budget_policy.rb
@@ -2,6 +2,10 @@ class BudgetPolicy < ApplicationPolicy
   def edit?
     record.building.user == user
   end
+
+  def destroy?
+    edit?
+  end
   class Scope < Scope
     def resolve
       scope.all
