6412110a9e4b26a92f4e81308679f26d7bb952d7
Daniel Lopez <dlopez.10g@gmail.com>
2018-09-07 15:22:51 -0500

    passing bill period on the controller

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -10,21 +10,29 @@ class BillsController < ApplicationController
     authorize @building, :building_of_current_user?
     params[:bills].each do |owner|
       next unless owner[:enviar].present?
-      create_bill(owner_params(owner))
+      create_bill(owner_params(owner), bill_date(bill_params(params)))
     end
   end
 
   private
 
-  def create_bill(owner_params)
+  def bill_params(params)
+    params.permit(:bill_period)
+  end
+
+  def create_bill(owner_params, period)
     @owner = Owner.find(owner_params[:owner_id])
     @owner.shares.each do |share|
       next if share.payment_percentage.zero?
-      Bill.create(share: share, status: "Pendiente")
+      Bill.create(share: share, status: "Pendiente", period: period)
     end
   end
 
   def owner_params(params)
     params.permit(:enviar, :owner_id)
+  end
+
+  def bill_date(date_params)
+    date_params[:bill_period]
   end
 end
