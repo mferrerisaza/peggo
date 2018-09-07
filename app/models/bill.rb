65c5a3902deed26ac4f8e8eab0a7db32a02a42cd
Miguel Ferrer <miguef7@gmail.com>
2018-09-06 21:24:27 -0500

    add after create to create bill concept with bill monthly fee

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,9 +1,17 @@
 class Bill < ApplicationRecord
-
+  after_create :add_property_monthly_fee
   enum status: ["Pagada", "Pendiente"]
 
   belongs_to :share
-  has_many :concepts
+  has_many :concepts, dependent: :destroy
 
   validates :status, presence: true
+
+  def add_property_monthly_fee
+    concept = Concept.new(
+      amount: share.bill_payment,
+      description: "Cuota administración #{share.owner.building.name} #{share.property.full_name}"
+    )
+    concepts << concept
+  end
 end
