8ceac7d1e243d472e7981069f6b1a0e4f1e8c901
Miguel Ferrer <miguef7@gmail.com>
2018-09-06 21:26:46 -0500

    add method for returning the sum of all share monthly payment

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,10 +1,16 @@
 class Owner < ApplicationRecord
   has_many :shares, dependent: :destroy
   has_many :properties, through: :shares
+  has_many :bills, through: :shares
   belongs_to :user
   belongs_to :building
 
   validates :name, :email, :card_number, presence: true
   validates :card_number, uniqueness: true
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP, message: "no válido"
+
+  def bill_amount
+    amounts = shares.map(&:bill_payment)
+    amounts.reduce(:+)
+  end
 end
