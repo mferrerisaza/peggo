0efdbab7e7ef74ea5c2b1edce74509db4105fb1a
Daniel Lopez <dlopez.10g@gmail.com>
2018-09-07 15:27:36 -0500

    add period on bills factory bot

--- spec/factories/bills.rb
+++ spec/factories/bills.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :bill do
     association :share
     status "Pendiente"
+    period Time.current.strftime("%Y-%m")
 
     trait :with_concepts do
       after(:create) { |bill| create_list(:concept, 5, bill: bill) }
