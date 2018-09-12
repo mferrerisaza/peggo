cd01cb4d40efdc6dd904888f3c0c80e6a2a4bcde
Daniel Lopez <dlopez.10g@gmail.com>
2018-09-11 21:36:40 -0500

    tests for bills math

--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -29,6 +29,49 @@ RSpec.describe Bill, type: :model do
     bill = FactoryBot.build(:bill, status: nil)
   end
 
+  it "returns 0 if payment_percentage is zero" do
+    @building = FactoryBot.create(:building, :with_active_budget, :with_propierties)
+    @owner = FactoryBot.create(:owner, building: @building)
+    @share = FactoryBot.create(:share, :with_bills, owner: @owner, payment_percentage: 0)
+    expect(@owner.owner_debt).to eq 0
+  end
+
+  context "bills math" do
+    before do
+      @building = FactoryBot.create(:building, :with_active_budget, :with_propierties)
+      @owner = FactoryBot.create(:owner, building: @building)
+      @share = FactoryBot.create(:share, owner: @owner)
+      @property = FactoryBot.create(:property, building: @building)
+      @bill = FactoryBot.create(:bill, share: @share)
+      @bill_paid = FactoryBot.create(:bill, share: @share)
+      @bill_paid.update(status: "Pagada")
+    end
+
+    it "returns the correct sum of amounts bill concepts" do
+      expect(@bill.sum_concepts_amount).to eq 10000000/12
+    end
+
+    it "returns the correct number of unpaid bills" do
+      expect(@share.unpaid_bills.size).to eq 1
+    end
+
+    it "return the correct amount of debt for a share" do
+      expect(@owner.owner_debt).to eq 10000000/12
+    end
+
+    it "return the correct amount of debt for and owner" do
+      expect(@owner.owner_debt).to eq 10000000/12
+    end
+
+    it "returns 0 if all bills are paid" do
+      @share = FactoryBot.create(:share, :with_bills, property: @property, owner: @owner, payment_percentage: 0)
+      @share.bills.each do |bill|
+        bill.status = "Pagada"
+      end
+      expect(@share.bills_debt).to eq 0
+    end
+  end
+
   context "concepts relationship" do
     it "can have many concepts" do
       building = FactoryBot.create(:building, :with_active_budget)
