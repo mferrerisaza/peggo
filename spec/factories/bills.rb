b8f0b5996e773b38b38abf254a2472c4ea6581d1
Daniel Lopez <dlopez.10g@gmail.com>
2018-09-11 21:35:54 -0500

    bill factory: change in period format creation

--- spec/factories/bills.rb
+++ spec/factories/bills.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :bill do
     association :share
     status "Pendiente"
-    period Time.current.strftime("%Y-%m")
+    sequence(:period) { |n| "#{Time.now.year}-0#{Time.now.month-(n-1)}" }
+    # period Time.current.strftime("%Y-%m")
 
     trait :with_concepts do
       after(:create) { |bill| create_list(:concept, 5, bill: bill) }
