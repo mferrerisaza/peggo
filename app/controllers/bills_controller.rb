1231edbee4c7b98d19e05bded3b371a8d4a3d1e0
Daniel Lopez <dlopez.10g@gmail.com>
2018-09-11 21:27:36 -0500

    create bill with month and year

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -10,29 +10,26 @@ class BillsController < ApplicationController
     authorize @building, :building_of_current_user?
     params[:bills].each do |owner|
       next unless owner[:enviar].present?
-      create_bill(owner_params(owner), bill_date(bill_params(params)))
+      create_bill(owner_params(owner), bill_params(params))
     end
+    redirect_to building_path(@building)
   end
 
   private
-
-  def bill_params(params)
-    params.permit(:bill_period)
-  end
-
-  def create_bill(owner_params, period)
-    @owner = Owner.find(owner_params[:owner_id])
-    @owner.shares.each do |share|
-      next if share.payment_percentage.zero?
-      Bill.create(share: share, status: "Pendiente", period: period)
-    end
-  end
 
   def owner_params(params)
     params.permit(:enviar, :owner_id)
   end
 
-  def bill_date(date_params)
-    date_params[:bill_period]
+  def bill_params(params)
+    params.permit(:bill_period)[:bill_period]
+  end
+
+  def create_bill(owner_params, bill_period)
+    @owner = Owner.find(owner_params[:owner_id])
+    @owner.shares.each do |share|
+      next if share.payment_percentage.zero?
+      Bill.create(share: share, status: "Pendiente", period: bill_period)
+    end
   end
 end
