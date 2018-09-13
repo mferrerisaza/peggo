d843d6727ccaba6528126ba5ba7706c8d83b1c0f
Daniel Lopez <dlopez.10g@gmail.com>
2018-09-13 11:52:38 -0500

    details

--- spec/factories/bills.rb
+++ spec/factories/bills.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     association :share
     status "Pendiente"
     sequence(:period) { |n| (Date.today - n.month).strftime("%Y/%m") }
-    # period Time.current.strftime("%Y-%m")
 
     trait :with_concepts do
       after(:create) { |bill| create_list(:concept, 5, bill: bill) }
