c429fd5ccec4e3e665631b3df3fc9d9aa63c74a8
Miguel Ferrer <miguef7@gmail.com>
2018-09-14 00:04:50 -0500

    add comment to fix future problem - resulting from merging features/mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@ class User < ApplicationRecord
     else
       user = User.new(user_params)
       user.password = Devise.friendly_token[0, 20] # Fake password for validation
+      # user.skip_confirmation! # uncomment when this branch gets merged with devise-confirmable
       user.save
     end
     return user
