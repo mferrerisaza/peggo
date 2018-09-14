39ad862c43b284f1b520d92d610c396e1f7c2e14
Miguel Ferrer <miguef7@gmail.com>
2018-09-13 23:48:49 -0500

    made tests work with confirmable

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,10 @@ FactoryBot.define do
     sequence(:email) { |n| "miguef#{n}@gmail.com" }
     password "123456"
 
+    after(:build) do |u|
+      u.skip_confirmation!
+    end
+
     trait :with_buildings do
       after(:create) { |user| create_list(:building, 2, user: user) }
     end
