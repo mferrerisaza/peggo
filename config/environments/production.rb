4aa3e7d8f4d4ccf87babb3b7fec4379c8695c995
Miguel Ferrer <miguef7@gmail.com>
2018-09-13 22:45:23 -0500

    add configr for action mailer in development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "http://www.peggo.com.co" }
+  config.action_mailer.delivery_method = :mailjet_api
+
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
