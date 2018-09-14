acd00f67cc2c4d4610860cce65aeb2c6eb0b950f
Miguel Ferrer <miguef7@gmail.com>
2018-09-14 14:41:17 -0500

    fix tests with new root page

--- spec/support/login_support.rb
+++ spec/support/login_support.rb
@@ -1,6 +1,9 @@
 module LoginSupport
   def sign_in_as(user)
     visit root_path
+    within ".banner-content" do
+      click_link "Iniciar Sesión"
+    end
     fill_in "Email", with: user.email
     fill_in "Contraseña", with: user.password
     click_button "Iniciar Sesión"
