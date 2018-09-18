b09462dd3bb7772044e94bbcfb037c34a4fe2556
Miguel Ferrer <miguef7@gmail.com>
2018-09-18 10:28:00 -0500

    add action for support method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
+  before_action :set_building, only: [:support]
 
   def home
+  end
+
+  def support
   end
 end
