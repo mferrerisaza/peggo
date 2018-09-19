85157ac894775324b0e85f2b59a391a888cd0cc1
Daniel Lopez <dlopez.10g@gmail.com>
2018-09-19 11:14:31 -0500

    show sidebar on bill index

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -5,7 +5,7 @@ module SidebarHelper
       properties: %w[index show new create edit update],
       owners: %w[index show new create edit update],
       shares: %w[create update],
-      bills: %w[new create errors],
+      bills: %w[index new create errors],
       budgets: %w[index new create edit update]
     }
     return if allowed_controllers[controller_name.to_sym].nil?
