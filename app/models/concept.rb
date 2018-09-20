d347c66e9b0b40b652aad4fe16f3482720ebc210
Daniel Lopez <dlopez.10g@gmail.com>
2018-09-19 20:21:46 -0500

    monetize amount paid

--- app/models/concept.rb
+++ app/models/concept.rb
@@ -1,7 +1,7 @@
 class Concept < ApplicationRecord
   belongs_to :bill
 
-  monetize :amount_cents
+  monetize :amount_cents, :amount_paid_cents
 
   validates :description, presence: true
   validates :description, uniqueness: { scope: :bill }
