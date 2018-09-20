adefbd9418217117cebca39366411fd5f4b970cd
Miguel Ferrer <miguef7@gmail.com>
2018-09-20 12:33:56 -0500

    fix attemp 1

--- app/models/concept.rb
+++ app/models/concept.rb
@@ -1,7 +1,8 @@
 class Concept < ApplicationRecord
   belongs_to :bill
 
-  monetize :amount_cents, :amount_paid_cents
+  monetize :amount_cents
+  monetize :amount_paid_cents
 
   validates :description, presence: true
   validates :description, uniqueness: { scope: :bill }
