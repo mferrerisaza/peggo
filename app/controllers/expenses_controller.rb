a5bd88985f8a00dbdc6e950f4cf816eca6622310
Miguel Ferrer <miguef7@gmail.com>
2019-05-27 22:30:32 -0500

    add new action

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,4 +4,10 @@ class ExpensesController < ApplicationController
   def index
     @expenses = policy_scope(Expense.where(building: @building).order(created_at: :asc))
   end
+
+  def new
+    @expense = Expense.new
+    authorize @building, :building_of_current_user?
+  end
+
 end
