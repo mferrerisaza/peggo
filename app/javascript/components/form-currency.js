1e0f5bf1f21b7092bcc837e746e1f3edf0edff82
Miguel Ferrer <miguef7@gmail.com>
2019-05-27 22:31:14 -0500

    refactor formatting method with cleave js

--- app/javascript/components/form-currency.js
+++ app/javascript/components/form-currency.js
@@ -1,22 +1,27 @@
-function replaceFormat(input) {
-  input.value = parseFloat(input.value.replace(/,/g, ""))
-                                        .toFixed(2)
-                                          .toString()
-                                            .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+// function replaceFormat(input) {
+//   input.value = parseFloat(input.value.replace(/,/g, ""))
+//                                         .toFixed(2)
+//                                           .toString()
+//                                             .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-}
+// }
 
-function addKeyPressListener(currencyInput) {
-  currencyInput.addEventListener("blur", (event) => {
-    replaceFormat(event.currentTarget);
-  })
-}
+// function addKeyPressListener(currencyInput) {
+//   currencyInput.addEventListener("keydown", (event) => {
+//     replaceFormat(event.currentTarget);
+//   })
+// }
 
+import Cleave from "cleave.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const currencyInput = document.getElementById("budget_amount");
-  if (currencyInput) {
-    addKeyPressListener(currencyInput)
-    replaceFormat(currencyInput)
+  const $amountInput = document.querySelector(".amount-input");
+  if($amountInput) {
+    const cleave = new Cleave('.amount-input', {
+        numeral: true,
+        numeralThousandsGroupStyle: 'thousand'
+    });
+
   }
 })
+
