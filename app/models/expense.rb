5898c7ba6b5d873e92e036eda7e7f413ed7ce8bb
Miguel Ferrer <miguef7@gmail.com>
2019-05-27 22:34:20 -0500

    add expense new form

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :building
+  enum payment_method: ["Tarjeta Crédito", "Banco", "Otro"]
   monetize :amount_cents
   validates :date, :description, presence: true
   validates :amount, numericality: { greater_than: 0 }
