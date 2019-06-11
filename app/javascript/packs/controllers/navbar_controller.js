3e86f56166587302efc87a8064c2aa914e9b76b1
Miguel Ferrer <miguef7@gmail.com>
2019-06-10 23:06:49 -0500

    refactoring

--- app/javascript/packs/controllers/navbar_controller.js
+++ app/javascript/packs/controllers/navbar_controller.js
@@ -5,16 +5,14 @@ export default class extends Controller {
   static targets = [ "whiteLogo", "colorLogo" ]
 
   scroll(){
-    if(this.element.classList.contains("navbar-wagon-transparent")) {
-      if (window.scrollY >= 100) {
-        this.element.classList.add('navbar-wagon-white');
-        this.whiteLogoTarget.classList.add("hidden");
-        this.colorLogoTarget.classList.remove("hidden");
-      } else {
-        this.element.classList.remove('navbar-wagon-white');
-        this.whiteLogoTarget.classList.remove("hidden");
-        this.colorLogoTarget.classList.add("hidden");
-      }
+    if (window.scrollY >= 100 && this.element.classList.contains("navbar-wagon-transparent")) {
+      this.element.classList.add('navbar-wagon-white');
+      this.whiteLogoTarget.classList.add("hidden");
+      this.colorLogoTarget.classList.remove("hidden");
+    } else {
+      this.element.classList.remove('navbar-wagon-white');
+      this.whiteLogoTarget.classList.remove("hidden");
+      this.colorLogoTarget.classList.add("hidden");
     }
   }
 }
