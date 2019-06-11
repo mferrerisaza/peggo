652f6128d9e545db3ec6d6f17c3eff65498345dd
Miguel Ferrer <miguef7@gmail.com>
2019-06-11 09:33:23 -0500

    cleaning

--- app/javascript/packs/controllers/navbar_controller.js
+++ app/javascript/packs/controllers/navbar_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   static targets = [ "whiteLogo", "colorLogo" ]
 
   scroll(){
-    if(this.element.classList.contains("navbar-wagon-transparent") {
+    if(this.element.classList.contains("navbar-wagon-transparent")) {
       if (window.scrollY >= 100) {
         this.element.classList.add('navbar-wagon-white');
         this.whiteLogoTarget.classList.add("hidden");
