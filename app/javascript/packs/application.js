e4d71950dc0b7623b179f4932478fdc5be62a096
Miguel Ferrer <miguef7@gmail.com>
2019-06-17 12:01:44 -0500

    add turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,9 @@
 import "bootstrap";
+import Turbolinks from 'turbolinks';
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
+Turbolinks.start();
