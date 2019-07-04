361d5bbca8b328e89639ee6be1865d8b8915d91f
Miguel Ferrer <miguef7@gmail.com>
2019-07-04 18:12:32 -0500

    invoice dates and items styling

--- app/javascript/packs/controllers/flatpickr_controller.js
+++ app/javascript/packs/controllers/flatpickr_controller.js
@@ -34,7 +34,30 @@ export default class extends Controller {
   }
 
   checkExpDate() {
-    console.log(this.element.value);
+    const $expDateInput = document.querySelector(".expiration-date");
+
+    if (this.element.value === "") {
+      const dateInputInvoice = flatpickr($expDateInput)
+      dateInputInvoice.destroy();
+      $expDateInput.classList.remove("datepicker-expense");
+      $expDateInput.value = "";
+      $expDateInput.disabled = true;
+    } else {
+      const invoiceDate = new Date(this.element.value);
+      $expDateInput.disabled = false;
+      $expDateInput.classList.add("datepicker-expense");
+
+      const dateInputInvoice = flatpickr($expDateInput, {
+        defaultDate: invoiceDate.fp_incr(30),
+        minDate: invoiceDate,
+        altInput: true,
+        altFormat: "d-m-Y",
+        dateFormat: "Y-m-d",
+        "locale": Spanish
+      });
+    }
+
+
   }
 
   get type() {
