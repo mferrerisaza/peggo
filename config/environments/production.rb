4a656526467b10d8a479b8b17d918024b19f0328
Miguel Ferrer <miguef7@gmail.com>
2019-07-09 14:14:23 -0500

    fix pdf css bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.precompile += ['pdf-to-pdf.scss']
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
