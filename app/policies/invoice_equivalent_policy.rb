9afe5277c0b3f6d41483fb2ec0fdf940f6616550
Miguel Ferrer <miguef7@gmail.com>
2019-07-16 22:21:15 -0500

    pundit policy for invoice equivalents

---
+++ app/policies/invoice_equivalent_policy.rb
@@ -0,0 +1,19 @@
+class InvoiceEquivalentPolicy < ApplicationPolicy
+  def show?
+    record.business.user == user
+  end
+
+  def destroy?
+    show?
+  end
+
+  def print?
+    show?
+  end
+
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+end
