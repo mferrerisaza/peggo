39613e4fa9a500c816791e781ffe5959405f8f86
Miguel Ferrer <miguef7@gmail.com>
2019-08-01 18:51:22 -0500

    enque job from create report action

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -2,9 +2,35 @@ class ExportsController < ApplicationController
   before_action :set_business
 
   def new
+    authorize @business, :business_of_current_user?
   end
 
   def create
-    raise
+    authorize @business, :business_of_current_user?
+    CreateAndMailReportJob.perform_now(
+      @business.id,
+      current_user.id,
+      export_params
+    )
+    flash[:notice] = "La generación del reporte puede tomar algún tiempo, una vez finalizado lo enviaremos a #{current_user.email}"
+    redirect_to new_business_export_path @business
+  end
+
+  private
+
+  def export_params
+    strong_params = params.require(:export).permit(
+      :dates,
+      :expense,
+      :invoice,
+      :invoice_equivalent,
+      :payment,
+      :contact
+    )
+    unless strong_params[:dates].blank?
+      dates = strong_params[:dates].split(" a ")
+      strong_params[:dates] = dates[0]..dates[1]
+    end
+    strong_params
   end
 end
