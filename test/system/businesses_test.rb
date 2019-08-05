7884e1513fcdc68352961575d5ffec7b20f9c17e
Miguel Ferrer <miguef7@gmail.com>
2019-08-05 13:56:22 -0500

    add business fixtures and two more tests for editing and deleting businesses

--- test/system/businesses_test.rb
+++ test/system/businesses_test.rb
@@ -11,4 +11,37 @@ class BusinessesTest < ApplicationSystemTestCase
       click_on "Crear"
     end
   end
+
+  test "let a signed in user edit an existing business" do
+    login_as users(:mike)
+    business = businesses(:xloop)
+    visit "/"
+    within ".card-actions" do
+      find('.edit-business').click
+    end
+    fill_in "Nombre de la empresa", with: "Scuad Editado"
+    page.attach_file("Logo", Rails.root + 'test/fixtures/files/SCUAD-final.png')
+
+    click_on "Editar"
+
+    business.reload
+
+    assert_equal "Scuad Editado", business.name
+    assert_not_nil business.logo
+  end
+
+  test "let a signed in user delete existing business" do
+    login_as users(:mike)
+    business = businesses(:xloop)
+    businesses_count = Business.count
+
+    visit "/"
+    within ".card-actions" do
+      accept_alert do
+        find('.delete-business').click
+      end
+    end
+    assert_selector ".alert.alert-info.alert-dismissible"
+    assert_equal businesses_count - 1, Business.count
+  end
 end
